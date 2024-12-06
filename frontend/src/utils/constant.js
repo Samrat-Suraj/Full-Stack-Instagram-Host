bf3ff51062f716d6000019e33dde04b85aa8f064
Suraj Maurya <samratsuraj87@gmail.com>
2024-12-06 22:17:30 +0530

    update

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,7 +1,7 @@
 
-export const USER_API_ENDPOINT  = "http://localhost:5000/api/v1/user"
-export const POST_API_ENDPOINT  = "http://localhost:5000/api/v1/post"
-export const MESSAGE_API_ENDPOINT  = "http://localhost:5000/api/v1/message"
+export const USER_API_ENDPOINT  = "https://insta-uy4u.onrender.com/api/v1/user"
+export const POST_API_ENDPOINT  = "https://insta-uy4u.onrender.com/api/v1/post"
+export const MESSAGE_API_ENDPOINT  = "https://insta-uy4u.onrender.com/api/v1/message"
 
 
 // User :---- 
